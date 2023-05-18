ee6a744f0951db509890eafd6310f7a4b0b7a093
vlee <vincentlee001@mail.fresnostate.edu>
2023-05-17 20:33:28 -0700

    Please enter the commit message for your changes. Lines starting

--- processor_top_tb.v
+++ processor_top_tb.v
@@ -9,6 +9,7 @@ module processor_top_tb;
 	initial begin
 		clk = 0;
 		enable = 1;
+		/*
 		instr = {6'b1000,5'b0, 5'b01, 16'b11};	//r1 = 4
 		#1
 		instr = {6'b1000,5'b0, 5'b10, 16'b10}; //r2 = 2
@@ -18,6 +19,18 @@ module processor_top_tb;
 		instr = {6'b0,5'b11,5'b10,5'b11,5'b0,6'b011000}; //r3 x r2 = r3
 		#1
 		instr = {6'b0,5'b11,5'b01,5'b100,5'b0,6'b100010}; //r3 - r1 = r4
+		#20
+		enable = 0;
+		*/
+		instr = {6'b1000,5'b0, 5'b10, 16'b100}; //addi r2, 4
+		#1
+		instr = {6'b0,5'b10,5'b10,5'b11,5'b0,6'b100000}; //add r3, r2, r2
+		#1
+		instr = {6'b101011, 5'b10, 5'b11, 16'b0}; //sw r3, 0(r2)
+		#1
+		instr = {6'b100011, 5'b10, 5'b01, 16'b0}; //lw r1, 0(r2)
+		#1
+		instr = {6'b0,5'b01,5'b11,5'b100,5'b0,6'b100100}; //and r4, r1, r3
 		#20
 		enable = 0;
 
